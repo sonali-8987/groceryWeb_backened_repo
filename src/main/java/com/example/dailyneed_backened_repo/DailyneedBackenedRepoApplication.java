661b81f52808e49f5456c66f256502731d0a6e22
Sonali Kumari <sonali.kumarithoughtworks.com@Sonalis-MacBook-Pro.local>
2022-08-29 08:40:31 -0700

    [Sonali] Add. SpringBoot Application

---
+++ src/main/java/com/example/dailyneed_backened_repo/DailyneedBackenedRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailyneed_backened_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyneedBackenedRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyneedBackenedRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dailyneed_backened_repo/DailyneedBackenedRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyneed_backened_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyneedBackenedRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
